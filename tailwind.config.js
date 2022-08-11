e68a10e651c09382f28b2736ba47acda1db0f3c9
Ahmed Mahfouz <ahmedmahfouz098@gmail.com>
2022-08-11 14:36:04 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./dist/*.html'],
+  screens: {
+    sm: '480px',
+    md: '768px',
+    lg: '976px',
+    xl: '1440px',
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Barlow Semi Condensed', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/line-clamp')],
+};
